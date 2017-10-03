d0a1a08ea8e2208251a4035b7fde2bd09a658adf
Chong Sun <sunchong137@gmail.com>
2017-10-02 22:24:36 -0700

    added a hubbard example

--- ftsolver/examples/hub1d_rhf.py
+++ ftsolver/examples/hub1d_rhf.py
@@ -13,10 +13,20 @@ def Hubbard1d(Nsite, Nelec, U, T,symm='RHF'):
 
 if __name__ == '__main__':
     
-    Nsite = 10
-    Nelec = 10
-    U = 1
-    T = np.linspace(0, 0.7,36)
-    E = Hubbard1d(Nsite,Nelec,U,T)
+    Nsite = 6
+    Nelec = Nsite
+    U = 4.0
+    h1e = np.zeros([Nsite,Nsite])
+    h2e = np.zeros((Nsite,)*4)
+    for i in range(Nsite):
+        h1e[i,(i+1)%Nsite] = -1.
+        h1e[i,(i-1)%Nsite] = -1.
+        h2e[i,i,i,i] = U
+
+    T = np.linspace(0, 1.10,56)
+    T = np.linspace(2.00, 3.00, 21)
+#    T = np.array([0.0, 1.0, 50.0])
+    E = Hubbard1d(Nsite,Nelec,U,T)/Nsite
     for i in range(len(T)):
-        print('%1.2f           %2.10f'%(T[i],E[i]))
+        print('%1.2f           %1.9f'%(T[i],E[i]))
+    
